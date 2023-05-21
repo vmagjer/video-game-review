be174ddb0434115f8532348e07acbb24f5e4186f
Vlatko Magjer <vlatko.magjer@gmail.com>
2023-05-21 21:12:59 +0200

    change basic e2e teset

--- tests/e2e/basic.spec.ts
+++ tests/e2e/basic.spec.ts
@@ -3,6 +3,5 @@ import { test, expect } from '@playwright/test'
 test('homepage details', async ({ page }) => {
   await page.goto('/')
 
-  await expect(page).toHaveTitle('Vite + Vue + TypeScript + Tailwind Starter Template')
-  await expect(page.locator('h2').first()).toHaveText('Hello World Component')
+  await expect(page).toHaveTitle('Game Search | VGR')
 })
