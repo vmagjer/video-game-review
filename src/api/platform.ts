de32764c84a3143f6fb612cd207ab4ff9fd4f00e
Vlatko Magjer <vlatko.magjer@gmail.com>
2023-05-21 18:21:31 +0200

    replace mock data with endpoint connections

--- src/api/platform.ts
+++ src/api/platform.ts
@@ -1,26 +1,12 @@
+import { API_URL } from "@/config"
+import axios from "axios"
 
 export type Platform = {
   id: number
   name: string
 }
 
-export const mockPlatforms: Platform[] = [
-  {
-    id: 1,
-    name: 'Platform 1',
-  },
-  {
-    id: 2,
-    name: 'Platform 2',
-  },
-  {
-    id: 3,
-    name: 'Platform 3',
-  },
-]
-
 export async function list(): Promise<Platform[]> {
-  // delay(500)
-  await new Promise(resolve => setTimeout(resolve, 500))
-  return mockPlatforms
+  const response = await axios.get(`${API_URL}/platform`)
+  return response.data
 }
---
+++ src/config.ts
@@ -0,0 +1,1 @@
+export const API_URL = 'https://video-game-review.onrender.com/api'
